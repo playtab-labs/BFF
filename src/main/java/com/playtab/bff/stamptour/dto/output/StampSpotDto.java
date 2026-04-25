5f501040056c72d431cb0fbd259311448e24070a
mikepark814-beep <mikepark814@pusan.ac.kr>
2026-04-25 22:02:11 +0900

    feat: 스탬프 투어 방문 요청 및 스팟에 위치 정보 추가

--- src/main/java/com/playtab/bff/stamptour/dto/output/StampSpotDto.java
+++ src/main/java/com/playtab/bff/stamptour/dto/output/StampSpotDto.java
@@ -7,6 +7,8 @@ public class StampSpotDto {
     private String spotDescription;
     private boolean visited;
     private String visitedAt;
+    private Double latitude;
+    private Double longitude;
 
     public Long getSpotId() {
         return spotId;
@@ -46,5 +48,21 @@ public class StampSpotDto {
 
     public void setVisitedAt(String visitedAt) {
         this.visitedAt = visitedAt;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
     }
 }
--- src/main/java/com/playtab/bff/stamptour/graphql/StampTourGraphqlController.java
+++ src/main/java/com/playtab/bff/stamptour/graphql/StampTourGraphqlController.java
@@ -1,7 +1,10 @@
 package com.playtab.bff.stamptour.graphql;
 
 import com.playtab.bff.stamptour.dto.output.MyStampsResponseDto;
+import com.playtab.bff.stamptour.dto.response.VisitResultDto;
 import com.playtab.bff.stamptour.service.StampTourFacade;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
@@ -17,5 +20,10 @@ public class StampTourGraphqlController {
     @QueryMapping
     public MyStampsResponseDto myStamps() {
         return stampTourFacade.getMyStamps();
+    }
+
+    @MutationMapping
+    public VisitResultDto visit(@Argument Long spotId, @Argument Double latitude, @Argument Double longitude) {
+        return stampTourFacade.visit(spotId, latitude, longitude);
     }
 }
